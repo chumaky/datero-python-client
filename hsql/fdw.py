9009fa2ec9adc9b8abb5030f694652a11e9c885a
Ameba Brain <ameba.brain@gmail.com>
2022-09-07 02:47:36 +0300

    switching to bind variable approach

--- hsql/fdw.py
+++ hsql/fdw.py
@@ -1,5 +1,7 @@
 """Foreign server management"""
+from typing import Dict, Tuple
 import psycopg2
+from psycopg2 import sql
 
 from .config import ConfigParser
 from .connection import Connection
@@ -14,7 +16,7 @@ class FDW:
     @property
     def servers(self):
         """List of foreign servers"""
-        #print(self.config)
+        ##print(self.config)
         return self.config['servers'] if 'servers' in self.config else {}
 
 
@@ -24,19 +26,25 @@ class FDW:
             cur = self.conn.cursor
 
             for server, props in self.servers.items():
-                #print(f'{server} - {props}')
-                options = ', '.join([f"{option} '{value}'" for option, value in props['options'].items()])
-                sql = \
-                    f'CREATE SERVER IF NOT EXISTS {server} ' \
-                    f"FOREIGN DATA WRAPPER {props['fdw_name']} " \
-                    f'OPTIONS ({options})'
+                stmt = \
+                    'CREATE SERVER IF NOT EXISTS {server} ' \
+                    'FOREIGN DATA WRAPPER {fdw_name} ' \
+                    'OPTIONS ({options})'
 
-                print(sql)
-                cur.execute(sql)
+                options, values = self._options_and_values(props['options'])
+
+                query = sql.SQL(stmt).format(
+                    server=sql.Identifier(server),
+                    fdw_name=sql.Identifier(props['fdw_name']),
+                    options=options
+                )
+
+                cur.execute(query, values)
                 self.conn.commit()
+                print(f'Foreign server "{server}" successfully created')
         except psycopg2.Error as e:
             self.conn.rollback()
-            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {sql}')
+            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {query.as_string(cur)}')
         finally:
             cur.close()
 
@@ -47,52 +55,97 @@ class FDW:
             cur = self.conn.cursor
 
             for server, props in self.servers.items():
-                #print(f'{server} - {props}')
-                options = ', '.join([f"{option} '{value}'" for option, value in props['user_mapping'].items()])
-                sql = \
-                    f'CREATE USER MAPPING IF NOT EXISTS FOR CURRENT_USER ' \
-                    f"SERVER {server} " \
-                    f'OPTIONS ({options})'
+                stmt = \
+                    'CREATE USER MAPPING IF NOT EXISTS FOR CURRENT_USER ' \
+                    'SERVER {server} ' \
+                    'OPTIONS ({options})'
 
-                print(sql)
-                cur.execute(sql)
+                options, values = self._options_and_values(props['user_mapping'])
+
+                query = sql.SQL(stmt).format(
+                    server=sql.Identifier(server),
+                    options=options
+                )
+
+                cur.execute(query, values)
                 self.conn.commit()
+                print(f'User mapping for "{server}" foreign server successfully created')
         except psycopg2.Error as e:
             self.conn.rollback()
-            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {sql}')
+            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {query.as_string(cur)}')
         finally:
             cur.close()
 
 
     def import_foreign_schema(self):
         """Import foreign schema"""
+
+        def recreate_schema():
+            query = sql.SQL('DROP SCHEMA IF EXISTS {local_schema} CASCADE') \
+                .format(local_schema=sql.Identifier(local_schema))
+
+            cur.execute(query)
+
+            query = sql.SQL('CREATE SCHEMA IF NOT EXISTS {local_schema}') \
+                .format(local_schema=sql.Identifier(local_schema))
+
+            cur.execute(query)
+
         try:
             cur = self.conn.cursor
 
             for server, props in self.servers.items():
-                #print(f'{server} - {props}')
+                ##print(f'{server} - {props}')
                 conf = props['import_foreign_schema']
                 remote_schema = conf['remote_schema']
                 local_schema = conf['local_schema']
 
-                sql = f'DROP SCHEMA IF EXISTS {local_schema} CASCADE; ' f'CREATE SCHEMA IF NOT EXISTS {local_schema}'
-                print(sql)
-                cur.execute(sql)
+                recreate_schema()
 
-                sql = \
-                    f'IMPORT FOREIGN SCHEMA "{remote_schema}" ' \
-                    f'FROM SERVER {server} ' \
-                    f'INTO {local_schema} '
+                stmt = \
+                    'IMPORT FOREIGN SCHEMA {remote_schema} ' \
+                    'FROM SERVER {server} ' \
+                    'INTO {local_schema} '
 
-                if 'options' in conf:
-                    options = ', '.join([f"{option} '{value}'" for option, value in conf['options'].items()])
-                    sql += f'OPTIONS(${options})'
+                # IMPORT FOREIGN SCHEMA doesn't accept an empty OPTIONS () clause
+                # hence we need to process separately cases with and without any options specified
+                if 'options' not in conf:
+                    query = sql.SQL(stmt).format(
+                        remote_schema=sql.Identifier(remote_schema),
+                        server=sql.Identifier(server),
+                        local_schema=sql.Identifier(local_schema),
+                    )
+                    cur.execute(query)
+                else:
+                    stmt += 'OPTIONS({options})'
 
-                print(sql)
-                cur.execute(sql)
+                    options, values = self._options_and_values(conf['options'])
+
+                    query = sql.SQL(stmt).format(
+                        remote_schema=sql.Identifier(remote_schema),
+                        server=sql.Identifier(server),
+                        local_schema=sql.Identifier(local_schema),
+                        options=options
+                    )
+                    cur.execute(query, values)
+
                 self.conn.commit()
+                print(f'Foreign schema "{remote_schema}" from server "{server}" successfully imported into "{local_schema}"')
         except psycopg2.Error as e:
             self.conn.rollback()
-            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {sql}')
+            print(f'Error code: {e.pgcode}, Message: {e.pgerror}' f'SQL: {query.as_string(cur)}')
         finally:
             cur.close()
+
+
+    def _options_and_values(self, options: Dict) -> Tuple[sql.SQL, Dict]:
+        """Internal method to prepare list of key-value options in a safe bind variables manner"""
+        keys = sql.SQL(', ').join([
+            sql.SQL(' ').join([sql.SQL(option), sql.Placeholder(option)])
+                for option in options.keys()
+        ])
+        values = {}
+        for option, value in options.items():
+            values[option] = str(value)
+
+        return (keys, values)
